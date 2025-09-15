72861723b8b91842dd110ca62fe4cd98ff6add4e
adewansyh1608 <2311523001_andre@student.unand.ac.id>
2025-09-15 11:28:01 +0700

    newwwww

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,6 @@
-
-const { User } = require('../models'); 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   const { nama, email, no_telepon, alamat, password } = req.body;
@@ -9,7 +8,7 @@ exports.register = async (req, res) => {
   try {
     const existingUser = await User.findOne({ where: { email } });
     if (existingUser) {
-      return res.status(400).json({ msg: 'Email sudah terdaftar.' });
+      return res.status(400).json({ msg: "Email sudah terdaftar." });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -20,27 +19,27 @@ exports.register = async (req, res) => {
       email,
       no_telepon,
       alamat,
-      role: 'user',
+      role: "user",
       password: hashedPassword,
     });
     const payload = {
       user: {
-        email: newUser.email, 
+        email: newUser.email,
       },
     };
 
     jwt.sign(
       payload,
-      process.env.JWT_SECRET, 
-      { expiresIn: '1h' },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
       (err, token) => {
         if (err) throw err;
-        res.json({ token });
+        res.render("login", { msg: "Registrasi berhasil. Silakan login." });
       }
     );
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
 
@@ -50,12 +49,12 @@ exports.login = async (req, res) => {
   try {
     const user = await User.findOne({ where: { email } });
     if (!user) {
-      return res.status(400).json({ msg: 'Email atau kata sandi salah.' });
+      return res.status(400).json({ msg: "Email atau kata sandi salah." });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ msg: 'Email atau kata sandi salah.' });
+      return res.status(400).json({ msg: "Email atau kata sandi salah." });
     }
     // 3. Jika cocok, buat payload untuk JWT
     const payload = {
@@ -67,7 +66,7 @@ exports.login = async (req, res) => {
     jwt.sign(
       payload,
       process.env.JWT_SECRET,
-      { expiresIn: '1h' },
+      { expiresIn: "1h" },
       (err, token) => {
         if (err) throw err;
         res.json({ token });
@@ -75,6 +74,6 @@ exports.login = async (req, res) => {
     );
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 };
