ce38cd2d304da5931c5c9bc3e90e1e2b7200556c
adewansyh1608 <2311523001_andre@student.unand.ac.id>
2025-09-17 07:53:57 +0700

    new push banget

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,23 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 exports.showReportForm = (req, res) => {
-    res.render('report-form', {
-        title: 'Form Laporan'
-    });
+  res.render("report-form", {
+    title: "Form Laporan",
+  });
 };
 
 exports.submitReport = (req, res) => {
-    const reportData = {
-        jenis_laporan: req.body.jenis_laporan,
-        nama_barang: req.body.nama_barang,
-        lokasi_kejadian: req.body.lokasi_kejadian,
-        tanggal_kejadian: req.body.tanggal_kejadian,
-        deskripsi: req.body.deskripsi,
-        foto_barang: req.file ? req.file.filename : null
-    };
+  const reportData = {
+    jenis_laporan: req.body.jenis_laporan,
+    nama_barang: req.body.nama_barang,
+    lokasi_kejadian: req.body.lokasi_kejadian,
+    tanggal_kejadian: req.body.tanggal_kejadian,
+    deskripsi: req.body.deskripsi,
+    foto_barang: req.file ? req.file.filename : null,
+  };
 
-    // Simpan ke database di sini
-    console.log('Report data:', reportData);
+  // Simpan ke database di sini
+  console.log("Report data:", reportData);
 
-    res.redirect('/report/success');
+  res.redirect("/mahasiswa/home");
 };
